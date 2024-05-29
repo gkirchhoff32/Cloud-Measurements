f2691a4db86b9550da13f4418b63dc2f7e668d7e
gkirchhoff32 <grant.kirchhoff@colorado.edu>
2024-05-29 17:36:49 -0600

    debugged simulator

--- library/plot_histogram.py
+++ library/plot_histogram.py
@@ -29,6 +29,7 @@ create_csv = False  # Set TRUE to generate a .csv from .ARSENL data
 load_data = True  # Set TRUE to load data into a DataFrame and serialize into a pickle object
 load_netcdf = True  # Set TRUE if loading from netcdf file ('*.ARSENL.nc'). Set FALSE if loading from *.ARSENL file.
 use_donovan = False  # Set TRUE if user wants to scale the histogram by using the Donovan correction
+limit_shots = True
 
 # window_bnd = [32e-9, 38e-9]  # [s] Set temporal boundaries for binning
 window_bnd = np.array([850, 1200])  # [m] Set boundaries for binning to exclude outliers
@@ -36,22 +37,31 @@ window_bnd = window_bnd / c * 2  # [s] Convert from range to tof
 dt = 25e-12  # [s] Resolution
 deadtime = 29.1e-9  # [s] Deadtime interval (25ns for sim, 29.1ns for SPCM)
 
-t_min = window_bnd[0]
-t_max = window_bnd[1]
+if limit_shots:
+    use_shots = 50
+
+t_min = window_bnd[0]  # [s]
+t_max = window_bnd[1]  # [s]
 
 if load_netcdf:
     home = str(Path.home())
     data_dir = home + r'\OneDrive - UCB-O365\ARSENL\Experiments\Cloud Measurements\Sims\saved_sims'
-    fname = r'\\simnum_4_nshot1.00E+03_useHGTrue.nc'
+    fname = r'\\simnum_7_nshot5.00E+02_useHGFalse.nc'
 
     ds = xr.open_dataset(data_dir + fname)
 
     cnts = ds.time_tag
+    if limit_shots:
+        lim_shot_idx = (abs(ds.time_tag_sync_index - use_shots)).argmin().values.item()
+        cnts = cnts[:lim_shot_idx]
+
     flight_time = cnts * dt  # [s]
     # Exclude specified t.o.f. bins
     flight_time = flight_time[np.where((flight_time >= window_bnd[0]) & (flight_time < window_bnd[1]))]
-
-    n_shots = len(ds.sync_index)
+    if limit_shots:
+        n_shots = len(ds.sync_index[:use_shots])
+    else:
+        n_shots = len(ds.sync_index)
 
 else:
     # Load INPHAMIS .ARSENL data if not yet serialized
@@ -81,7 +91,7 @@ else:
 ### Histogram of time of flight ###
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-bin_avg = 50
+bin_avg = 500
 res = dt * bin_avg
 bin_array = set_binwidth(t_min, t_max, res)
 # bin_array = set_binwidth(t_min, t_max, dt)
@@ -89,21 +99,18 @@ n, bins = np.histogram(flight_time, bins=bin_array)
 print('Histogram plot time elapsed: {:.3} sec'.format(time.time() - start))
 binwidth = np.diff(bins)[0]
 N = n / binwidth / n_shots
-print(n_shots)
+print('Number of shots: {}'.format(n_shots))
 if use_donovan:
     N_dono = N / (1 - N*deadtime)
 center = 0.5 * (bins[:-1]+bins[1:])
-ax1.bar(center, N, align='center', width=binwidth, color='b', alpha=0.75, label='Detections')
+ax1.bar(center*c/2, N, align='center', width=binwidth*c/2, color='b', alpha=0.75, label='Detections')
 if use_donovan:
-    ax1.bar(center, N_dono, align='center', width=binwidth, color='r', alpha=0.5, label='Muller "Corrected" Profile')
-    # ax1.set_ylim([-500, 750])
-    # ax1.set_xlim([2.9e-8, 3.3e-8])
+    ax1.bar(center*c/2, N_dono, align='center', width=binwidth*c/2, color='r', alpha=0.5, label='Muller "Corrected" Profile')
     ax1.set_title('Inaccurate Muller Correction Demonstration')
     plt.legend()
-ax1.set_xlabel('Time of flight [s]')
+ax1.set_xlabel('Range [m]')
 ax1.set_ylabel('Arrival rate [Hz]')
 ax1.set_yscale('log')
-# ax1.set_title('Wall Retrieval')
 plt.tight_layout()
 plt.show()
 
