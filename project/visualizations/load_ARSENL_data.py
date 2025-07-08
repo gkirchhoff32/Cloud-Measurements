36419a67ed1d0d2a8d76e32950762bf544a0c56d
gkirchhoff32 <grant.kirchhoff@colorado.edu>
2025-07-08 15:54:40 -0600

    starting to look into plotting

---
+++ project/visualizations/load_ARSENL_data.py
@@ -0,0 +1,75 @@
+#
+# ARSENL Backscatter Experiments
+# load_ARSENL_data.py
+#
+# Grant Kirchhoff
+# Rev. 1: 02-25-2022 Initial loading from raw data
+# Rev. 2: 06-24-2022 Loading from netcdf format
+# Updated 07-01-2025 Checking first light from CoBaLT Lidar data
+# University of Colorado Boulder
+#
+"""
+
+Load data from ARSENL INPHAMIS lidar as a DataFrame and serialize as a pickle file. The purpose is to load the large
+dataset as a pickle object without having to reload the data every time main() is executed. This reduces execution time
+and enables flexibility with the data handling.
+
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import time
+import pickle
+
+# Constants
+c = 299792458  # [m/s] Speed of light
+
+# data_dir = r'/home/arsenl/inphamis-acquisition/two_picos_v2/CoBaLT_data'
+data_dir = r'/home/arsenl/inphamis-acquisition/two_picos_v2/CoBaLT_data'
+fname = r'/Dev_0_-_2025-07-07_22.08.23.ARSENL'
+picklename = 'test'
+
+def load_INPHAMIS_data(data_dir, fname, picklename):
+    """
+    Loads data from INPHAMIS lidar acquisition system into DataFrame object and stores it as a serialized pickle
+    object for fast data loading.
+    :param fname: filename to be loaded [str]
+    :param picklename: filename of pickle [str]
+    :param create_csv: whether or not to create a csv file from the data [bool]
+    """
+
+    start = time.time()
+    df = pd.read_csv(data_dir + fname, delimiter=',')
+    print('Elapsed time (read pd): {} sec'.format(time.time()-start))
+
+    outfile = open('{}/{}'.format(data_dir, picklename), 'wb')
+    pickle.dump(df, outfile)
+    outfile.close()
+
+    return df
+
+def set_binwidth(tmin, tmax, resolution):
+    bin_array = np.arange(tmin, tmax+resolution, resolution)
+    return bin_array
+
+def get_times(df, exclude):
+    df1 = df.loc[df['dtime'] != 0]
+    detect = df1.loc[(df1['overflow'] == 0) & (df1['channel'] == 0)]  # Return data for detection event ("overflow","channel" = 0,0)
+    sync = df1.loc[(df1['overflow'] == 1) & (df1['channel'] == 0)]
+    n_shots = len(sync)
+    
+    sync_detect_idx = np.array(detect.index) - 1  # Extract index immediately prior to detection event to match with laser pulse
+    sync_detect = df.loc[sync_detect_idx]  # Laser pulse event prior to detection event
+    
+    detect_time = detect['dtime'].to_numpy()
+    sync_detect_time = sync_detect['dtime'].to_numpy()
+    
+    flight_time = (detect_time - sync_detect_time) * 25  # [ps] Time is in segments of 25 ps
+    flight_time = flight_time[np.where((flight_time >= exclude[0]) & (flight_time < exclude[1]))]  # Exclude t.o.f. where bins ~= 0
+    distance = flight_time / 1e12 * c / 2
+
+    return flight_time, n_shots
+
+
+
---
+++ project/visualizations/plot_histogram_updated.py
@@ -0,0 +1,99 @@
+#
+# ARSENL Backscatter Experiments
+# plot_histogram.py
+#
+# Grant Kirchhoff
+# 02-25-2022
+# University of Colorado Boulder
+#
+# Updated: 07-01-2025
+
+"""
+Histogram photon arrival time data from ARSENL INPHAMIS lidar. IMPORTANT: Set data path settings in
+'load_ARSENL_data.py' first.
+"""
+
+import numpy as np
+import time
+import pickle
+import matplotlib.pyplot as plt
+
+from load_ARSENL_data import load_INPHAMIS_data, set_binwidth, data_dir, fname, picklename
+
+start = time.time()
+# Constants
+c = 299792458  # [m/s] Speed of light
+
+# Parameters
+create_csv = False  # Set TRUE to generate a .csv from .ARSENL data
+load_data = True  # Set TRUE to load data into a DataFrame and serialize into a pickle object
+#exclude = [27500, 33500]  # [ps] Set temporal boundaries for binning
+exclude = [0, 70e6]  # [ps] temporal binning bounds
+binsize = 12e-8  # [s] bin width for plotting
+
+# Load INPHAMIS .ARSENL data if not yet serialized
+if load_data:
+    load_INPHAMIS_data(data_dir, fname, picklename)
+
+# Unpickle the data to DataFrame object
+infile = open('{}/{}'.format(data_dir, picklename), 'rb')
+df = pickle.load(infile)
+infile.close()
+
+df1 = df.loc[df['dtime'] != 0]
+detect = df1.loc[(df1['overflow'] == 0) & (df1['channel'] == 0)]  # Return data for detection event ("overflow","channel" = 0,0)
+sync = df1.loc[(df1['overflow'] == 1) & (df1['channel'] == 0)]  # sync detection (laser pulse) ("overflow", "channel" = 1,0)
+rollover = df1.loc[(df1['overflow'] == 1) & (df1['channel'] == 63)]  # Clock rollover ("overflow", "channel" = 1,63)
+n_shots = len(sync)
+
+# Clean up data first
+start_idx = sync.index[0]  # first index where laser is recorded
+detect = detect[detect.index > start_idx]  # throw away detections that precede the first laser shot
+for i in rollover.index:
+    # return closest sync that comes after it
+
+    sync_rollover_idx = sync[sync.index > i].index[0]
+    detect_rollover_idx = detect[(detect.index > i) & (detect.index < sync_rollover_idx)].index
+    rollover_dtime = rollover.loc[i]['dtime']
+    print(rollover_dtime)
+    print(i)
+    print(sync_rollover_idx)
+    print(detect_rollover_idx)
+
+    print(detect.loc[detect_rollover_idx]['dtime'])
+
+    quit()
+
+#print(rollover)
+
+quit()
+
+
+
+sync_detect_idx = np.array(detect.index) - 1  # Extract index immediately prior to detection event to match with laser pulse
+sync_detect = df.loc[sync_detect_idx]  # Laser pulse event prior to detection event
+
+detect_time = detect['dtime'].to_numpy()
+sync_detect_time = sync_detect['dtime'].to_numpy()
+
+flight_time = (detect_time - sync_detect_time) * 25  # [ps] Time is in segments of 25 ps
+flight_time = flight_time[np.where((flight_time >= exclude[0]) & (flight_time < exclude[1]))]  # Exclude t.o.f. where bins ~= 0
+distance = flight_time / 1e12 * c / 2
+
+### Histogram of time of flight ###
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+bin_array = set_binwidth(exclude[0]*1e-12, exclude[1]*1e-12, binsize)
+n, bins = np.histogram(flight_time*1e-12, bins=bin_array)
+print('Histogram time elapsed: {:.3} sec'.format(time.time() - start))
+binwidth = np.diff(bins)[0]
+N = n / binwidth / n_shots
+center = 0.5 * (bins[:-1]+bins[1:])
+ax1.bar(center*1e6, N, align='center', width=binwidth*1e6, color='b', alpha=0.75)
+ax1.set_xlabel('Time of flight [us]')
+ax1.set_ylabel('Arrival rate [Hz]')
+ax1.set_xlim([0, 60])
+ax1.set_title('Time of flight for INPHAMIS backscatter')
+plt.tight_layout()
+plt.show()
+
