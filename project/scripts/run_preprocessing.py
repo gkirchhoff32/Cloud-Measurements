3902fbab2028424b3198acb1af424ce5bc96f565
gkirchhoff32 <grant.kirchhoff@colorado.edu>
2025-09-22 18:26:46 -0600

    was able to run code from scipts folder

---
+++ project/scripts/run_preprocessing.py
@@ -0,0 +1,33 @@
+"""
+run_preprocessor.py
+
+script to run the preprocessing routine from "data_preprocessor.py" which converts the timestamps from the binary format .ARSENL file to ranges
+and laser shots. Plot the data in scatter or histogram format.
+
+09.19.2025
+"""
+
+import yaml
+from pathlib import Path
+
+from project.processing.data_preprocessor import DataPreprocessor
+
+def main():
+    """
+    Load .ARSENL file --> preprocess to netcdf file --> generate and display histogram or display scatter plot
+    """
+
+    config_path = Path(__file__).resolve().parent.parent / "config" / "preprocessing.yaml"
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+
+    dp = DataPreprocessor(config)
+    dp.preprocess()
+    if dp.histogram:
+        histogram_results = dp.gen_histogram()
+        dp.plot_histogram(histogram_results)
+    else:
+        dp.plot_scatter()
+
+if __name__ == '__main__':
+    main()
