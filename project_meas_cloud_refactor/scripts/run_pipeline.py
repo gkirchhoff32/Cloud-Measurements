d236abc736f977ca68d9f98c9b87ff1eacd50dc6
Grant Kirchhoff <gkirchhoff32@gmail.com>
2026-02-25 18:34:33 -0700

    added plot to rain paper

--- project_meas_cloud_refactor/scripts/run_pipeline.py
+++ project_meas_cloud_refactor/scripts/run_pipeline.py
@@ -15,7 +15,7 @@ from simulation.gen_sim_data import GenerateSimData
 from utils.array_utils import bootstrap
 
 use_sim = False
-process = True
+process = False
 degree_start = 8
 degree_end = 25
 
@@ -47,13 +47,13 @@ def run():
         ranges, shots_time = ncl.load_chunks(preprocess_path, generic_fname)
 
     dpl = DataPlotter(config)
-    # dpl.plot_time_tag_scatter(ranges, shots_time, timestamp, low_gain, generic_fname)
+    dpl.plot_time_tag_scatter(ranges, shots_time, timestamp, low_gain, generic_fname)
 
-    gh = GenerateHistogram(config)
-    r_binedges, t_binedges, flux, H = gh.gen_histogram(ranges, shots_time, low_gain)
-    dpl.plot_histogram(flux, t_binedges, r_binedges, timestamp, low_gain, generic_fname)
-    H_train, H_val = bootstrap(H)
-    # quit()
+    # gh = GenerateHistogram(config)
+    # r_binedges, t_binedges, flux, H = gh.gen_histogram(ranges, shots_time, low_gain)
+    # dpl.plot_histogram(flux, t_binedges, r_binedges, timestamp, low_gain, generic_fname)
+    # H_train, H_val = bootstrap(H)
+    # # quit()
 
     if process:
         dp = DeadtimeProcessing(config)
