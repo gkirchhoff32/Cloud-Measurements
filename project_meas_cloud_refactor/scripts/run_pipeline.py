2a1d13075fd3ae826cd4e485a2557d7e53cc17a7
Grant Kirchhoff <gkirchhoff32@gmail.com>
2026-06-26 17:34:28 -0600

    plots for ilrc

--- project_meas_cloud_refactor/scripts/run_pipeline.py
+++ project_meas_cloud_refactor/scripts/run_pipeline.py
@@ -15,7 +15,7 @@ from simulation.gen_sim_data import GenerateSimData
 from utils.array_utils import bootstrap
 
 use_sim = False
-process = False
+process = True
 histogram = True
 degree_start = 2
 degree_end = 8
