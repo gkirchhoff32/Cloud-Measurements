229ff6a329b1bc58980e79c41d5cfddead60385a
Grant Kirchhoff <gkirchhoff32@gmail.com>
2026-09-18 19:24:10 -0500

    looking at old simulation code and creating inheritances in dissertation repo

--- project_meas_cloud_refactor/scripts/run_pipeline.py
+++ project_meas_cloud_refactor/scripts/run_pipeline.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 import yaml
 
-# Add the project root directory to Python path
+# Add the project root directory to Python path 
 project_root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(project_root))
 
